88ca57ae66f8cc157a2a0d20acbd544b26c38107
Kevin Sandom <ksandom40@yahoo.com.br>
2012-03-24 20:33:00 +0000

    Added the ability to flatten nesting with --flatten.

--- modules-available/manipulator.php
+++ modules-available/manipulator.php
@@ -18,14 +18,15 @@ class Manipulator extends Module
 		{
 			case 'init':
 				$this->core->registerFeature($this, array('toString'), 'toString', 'Convert array or arrays into an array of strings.');
+				$this->core->registerFeature($this, array('f', 'flatten'), 'flatten', 'Flatten an array or arrays into a keyed array of values. --flatten[=limit]. Note that "limit" specifies how far to go into the nesting before simply returning what ever is below.');
 				break;
-				$this->core->registerFeature($this, array('f', 'flatten'), 'flatten', 'Flatten an array or arrays into a keyed array of values.');
+			case 'followup':
 				break;
 			case 'toString':
 				return $this->toString($this->core->getSharedMemory());
 				break;
 			case 'flatten':
-				return $this->flatten($this->core->getSharedMemory());
+				return $this->flatten($this->core->getSharedMemory(), $this->core->get('Global', 'flatten'));
 				break;
 			default:
 				$this->core->complain($this, 'Unknown event', $event);
@@ -38,25 +39,39 @@ class Manipulator extends Module
 		return $input;
 	}
 	
-	function flatten($input)
+	function flatten($input, $limit, $nesting=0)
 	{
-		return $input;
+		$output=array();
+		$clashes=array();
+		$this->getArrayNodes($output, $input, $clashes, $limit, $nesting);
+		return $output;
 	}
 	
-	private function getArrayNodes(&$output, $input, &$clashes)
+	private function getArrayNodes(&$output, $input, &$clashes, $limit, $nesting)
 	{
+		echo "$limit, $nesting\n";
 		foreach ($input as $key=>$value)
 		{
-			if (is_numeric($key)) $output[]=$value;
+			if (is_array($value) and !(is_numeric($limit) and ($nesting>=$limit)))
+			{
+				$this->getArrayNodes($output, $value, $clashes, $limit, $nesting+1);
+			}
 			else
 			{
-				if (!isset($output[$key])) $output[$key]=$value;
+				if (is_numeric($key)) $output[]=$value;
 				else
 				{
-					# work out new key based on clashes
+					if (!isset($output[$key])) $output[$key]=$value;
+					else
+					{
+						# work out new key based on clashes
+						$clashes[$key]=(isset($clashes[$key]))?$clashes[$key]+1:1;
+						$newKey="$key{$clashes[$key]}";
+						echo "Chose key $newKey\n";
+						$output[$newKey]=$value;
+					}
 					
 				}
-				
 			}
 		}
 	}
