50c46434b8c197bb9763ee4d8951efa0152fe4c7
Kevin Sandom <ksandom40@yahoo.com.br>
2012-03-25 00:28:11 +0000

    Added exec

---
+++ modules-available/exec.php
@@ -0,0 +1,51 @@
+<?php
+# Copyright (c) 2012, Kevin Sandom under the BSD License. See LICENSE for full details.
+
+# Run stuff!
+
+class Exec extends Module
+{
+	private $dataDir=null;
+	
+	function __construct()
+	{
+		parent::__construct('Exec');
+	}
+	
+	function event($event)
+	{
+		switch ($event)
+		{
+			case 'init':
+				$this->core->registerFeature($this, array('e', 'exec'), 'exec', 'Blindly execute what ever we recieve. USE WITH CARE!"');
+				break;
+			case 'followup':
+				break;
+			case 'exec':
+				$this->exec($this->core->getSharedMemory());
+				break;
+			default:
+				$this->core->complain($this, 'Unknown event', $event);
+				break;
+		}
+	}
+	
+	function exec($input)
+	{
+		foreach ($input as $line)
+		{
+			if (is_array($line)) $this->run($line);
+		}
+	}
+	
+	function run($line)
+	{
+		return `$line`;
+	}
+	
+}
+
+$core=core::assert();
+$core->registerModule(new Exec());
+ 
+?>
