0e36b9493d1ba8a90d8c59b80e52f894678bc804
Kevin Sandom <kevinsandom@yahoo.co.uk>
2012-06-02 14:02:07 +0100

    Updated colour choices after comments about readability from sweeny. Should be better now :)

--- modules-available/commandLine.php
+++ modules-available/commandLine.php
@@ -182,7 +182,7 @@ class CommandLine extends Module
 			$objName=$details['obj']->getName();
 			
 			$this->assertCodes();
-			echo "{$this->codes['brightBlack']}$objName: {$this->codes['default']}$finalVisualFlags {$this->codes['brightBlack']}=> {$this->codes['cyan']}{$details['description']}{$this->codes['default']}\n";
+			echo "{$this->codes['brightBlack']}$objName: {$this->codes['cyan']}$finalVisualFlags {$this->codes['brightBlack']}=> {$this->codes['default']}{$details['description']}{$this->codes['default']}\n";
 		}
 	}
 	
