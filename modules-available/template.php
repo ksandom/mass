d6d957341b073ef36b51233554d73a8bc39ba120
Kevin Sandom <kevinsandom@yahoo.co.uk>
2012-10-02 10:11:49 +0100

    More work on nestTemplates. It now fully works. Began on nestTemplatesOut

--- modules-available/template.php
+++ modules-available/template.php
@@ -28,6 +28,7 @@ class Template extends Module
 				$this->core->registerFeature($this, array('unsetTemplateOut'), 'unsetTemplateOut', 'Unset the current templateOut. This disables the output, but allows --templateOutIfNotSet to be used again.');
 				$this->core->registerFeature($this, array('templateOutIfNotSet'), 'templateOutIfNotSet', "Same as --templateOut, but will only be set if it hasn't been already.");
 				$this->core->registerFeature($this, array('nestTemplates'), 'nestTemplates', "Nest templates. --nestTemplates=rootTemplate,[inputField],[outputField],firstNestedTemplate,[inputField],[outputField][,secondNestedTemplate,[inputField],[outputField][,thirdNestedTemplate,[inputField],[outputField][,etc[,etc]]]] . The syntax works in sets of 3. The first field is the template to use. The second is the field to pass as the input to the next template. If the input is omitted, the whole level will be passed to the next template. The third is the field to put the result from the next template into. If ommitted the whole level will be replaced by a string that can be accessed via ~%line%~ .");
+				$this->core->registerFeature($this, array('nestTemplatesOut'), 'nestTemplatesOut', "Same as nestTemplates, but will use the output object instead.");
 				
 				$this->loadEnabledTenmplates();
 				break;
@@ -67,6 +68,9 @@ class Template extends Module
 			case 'nestTemplates':
 				$parms=$this->core->interpretParms($this->core->get('Global', $event), 3, 1);
 				return array($this->nestTemplates($this->core->getResultSet(), $parms[0], $parms[1], $parms[2], $parms[3]));
+				break;
+			case 'nestTemplatesOut':
+				$this->templateOut=$this->core->get('Global', $event);
 				break;
 			default:
 				$this->core->complain($this, 'Unknown event', $event);
@@ -201,7 +205,10 @@ class Template extends Module
 		}
 		else
 		{
-			return '	'.implode("\n	", explode("\n", $dataIn));
+			$output='	'.implode("\n	", explode("\n", $dataIn));
+			$length=strlen($output);
+			if (substr($output, $length-1, 1)=='	') $output=substr($output, 0, $length-1);
+			return $output;
 		}
 	}
 	
@@ -216,23 +223,31 @@ class Template extends Module
 			{
 				if ($input)
 				{
-					if (isset($line[$input])) $outputLine=$this->core->callFeatureWithDataset('nestTemplates', $remainder, $line[$input], $autoIndent);
+					if (isset($line[$input]))
+					{
+						$outputLine=$this->core->callFeatureWithDataset('nestTemplates', $remainder, $line[$input], $autoIndent);
+						$this->core->debug(2, "nestTemplates: Using input $input $remainder");
+					}
 					else
 					{
 						$this->core->debug(2, "nestTemplates: Input key $input did not exist when trying to process template the remaining part of nestTemplates sequence $remainder.");
 						$outputLine=false;
 					}
 				}
-				else $outputLine=$this->core->callFeatureWithDataset('nestTemplates', $remainder, $line);
+				else
+				{
+					$this->core->debug(2, "nestTemplates: Taking the whole array for $remainder");
+					$outputLine=$this->core->callFeatureWithDataset('nestTemplates', $remainder, $line);
+				}
 				
 				if ($outputLine!==false)
 				{
 					$outputLine=$this->indent($outputLine);
-					
 					if ($output) $dataOut[$key][$output]=$outputLine[0];
 					else $dataOut[$key]=$outputLine[0];
 				}
 			}
+			$this->core->debug(2, "nestTemplates: Finished remainder $remainder");
 		}
 		
 		return $this->processTemplateByName($templateName, $dataOut);
@@ -241,6 +256,10 @@ class Template extends Module
 	function out($output)
 	{
 		if (is_string($output)) echo "template: Unexpected string=\"$output\"\n";
+		elseif(strpos($this->templateOut, ',')!==false)
+		{
+			echo $this->core->callFeatureWithDataset('nestTemplates', $this->templateOut, $output);
+		}
 		else
 		{
 			echo $this->processTemplateByName($this->templateOut, $output);
