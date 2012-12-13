4a8d9c9529271069cd7dd46ddf8c8e23c15c244a
Kevin Sandom <kevinsandom@yahoo.co.uk>
2012-12-13 20:43:32 +0000

    Install: Further tidying.

--- install.sh
+++ install.sh
@@ -14,10 +14,11 @@ programName='mass'
 fileThings='macros modules templates'
 directoryThings='packages'
 things="$fileThings $directoryThings"
+installTypeComments=''
 
 function userInstall
 {
-	echo "Non root install chosen"
+	# echo "Non root install chosen"
 	configDir=~/.$programName
 	bin="$configDir/bin"
 	binExec=~/bin
@@ -27,7 +28,7 @@ function userInstall
 
 function rootInstall
 {
-	echo "Root install chosen"
+	# echo "Root install chosen"
 	configDir="/etc/$programName"
 	bin="/usr/bin"
 	binExec=/usr/bin
@@ -39,7 +40,7 @@ function rootInstall
 function checkPrereqs
 {
 	for prereq in php;do
-		if ! which $prereq; then
+		if ! which $prereq 1>/dev/null; then
 			echo "Could not find $prereq in \$PATH" >&2
 			exit 1
 		fi
@@ -48,8 +49,9 @@ function checkPrereqs
 
 function linkedInstall
 {
-	echo "Linked install chosen"
-	echo "NOTE that this is a linked install so you need to keep the reposittory that you installed from in place. If you don't want to do this, you may want to consider installing as root, which will make it available to all users."
+	# echo "Linked install chosen"
+	installTypeComments="This is a linked install so you need to keep the repository that you installed from in place. 
+	If you don't want to do this, you may want to consider installing as root, which will make it available to all users."
 	
 	configDir=~/.$programName
 	bin="."
@@ -180,7 +182,6 @@ function removeObsoleteStuff
 	mv $configDir/obsolete/*available $configDir
 	
 	if [ `ls $configDir/obsolete 2> /dev/null | wc -l` -lt 1 ]; then
-		echo "removeObsoleteStuff: No obsolete stuff to keep. Removing $configDir/obsolete."
 		rmdir $configDir/obsolete
 	else
 		echo "removeObsoleteStuff: Obsolete stuff has been put in $configDir/obsolete. It is likely that this directory can simply be deleted. But if you have done any custom work, you will want to check that it isn't here first." | tee $configDir/obsolete/readme.md
@@ -196,38 +197,37 @@ function doInstall
 	removeObsoleteStuff
 	
 	echo "Install details:
-	What: $programName
-	Where:
-		config: $configDir
-		bin: $bin
-		binExec: $binExec
-		startDir: $startDir
-		installType: $installType
-		"
+	what: $programName
+	config: $configDir
+	bin: $bin
+	binExec: $binExec
+	startDir: $startDir
+	installType: $installType
+	installNotes: $installTypeComments"
 	
 	if [ "$installType" == 'cp' ]; then
-		echo -e "Copying available stuff"
+		# echo -e "Copying available stuff"
 		cp -Rv docs templates-* modules-* core.php macros-* packages-* examples interfaces supplimentary index.php "$configDir"
 		
-		echo -e "Setting up remaining directory structure"
+		# echo -e "Setting up remaining directory structure"
 		cd "$configDir"
 		mkdir -p config data/1LayerHosts
 		
-		echo -e "Making the thing runnable"
+		# echo -e "Making the thing runnable"
 		cd $binExec
 		cp -Rv "$startDir/$programName" "$bin"
 		chmod 755 "$bin/$programName"
 	else
 		cd "$configDir"
 		
-		echo -e "Linking like there's no tomorrow."
+		# echo -e "Linking like there's no tomorrow."
 		ln -sf "$startDir"/docs "$startDir"/modules-*available "$startDir"/macros-*available "$startDir"/templates-*available "$startDir/core.php" "$startDir/examples" "$startDir"/packages-*available "$startDir"/interfaces "$startDir"/supplimentary . 
 		ln -sf "$startDir/index.php" .
 		
-		echo -e "Setting up remaining directory structure"
+		# echo -e "Setting up remaining directory structure"
 		mkdir -p config data/1LayerHosts
 		
-		echo -e "Making the thing runnable"
+		# echo -e "Making the thing runnable"
 		cd $binExec
 		ln -sf "$startDir/$programName" .
 	fi
@@ -245,7 +245,7 @@ function doInstall
 	disableItemInProfile publicWebAPI packages AWS
 	cleanProfile publicWebAPI
 	
-	echo -e "Cleanup"
+	# echo -e "Cleanup"
 	rm -f "$configDir/macros-enabled/example"*
 	rm -f "$configDir/modules-enabled/example"
 	rm -f "$configDir/templates-enabled/example"
