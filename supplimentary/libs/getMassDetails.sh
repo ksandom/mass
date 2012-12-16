2214dc740ce5f3bb633b042eb32a579d11bee432
Kevin Sandom <kevinsandom@yahoo.co.uk>
2012-12-16 21:37:42 +0000

    Added documentation to libraries.

--- supplimentary/libs/getMassDetails.sh
+++ supplimentary/libs/getMassDetails.sh
@@ -1,4 +1,6 @@
 # Get mass details
+# See documentation inside includeLibs.sh to use this library
+
 configDir=`mass --get=General,configDir --singleStringNow --null`
 libDir=configDir/supplimentary/libs
 
--- supplimentary/libs/getRepo.sh
+++ supplimentary/libs/getRepo.sh
@@ -1,9 +1,10 @@
 # Get a repo
+# Install/update a repo
 
 function getRepo
 {
-	repoName="$1"
-	repoSrc="$2"
+	repoName="$1" # What we are going to refer to it as once it's installed.
+	repoSrc="$2" # Where to get it from. This is likely to be a git URL.
 	libDir="$configDir/externalLibraries"
 	repoDir="$libDir/$repoName"
 	
