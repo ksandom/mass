68e02f4d104e173377523b7804e41e615810d328
Kevin Sandom <kevinsandom@yahoo.co.uk>
2013-10-04 00:58:55 +0100

    repoParms: Lots more work laying the foundations.

--- supplimentary/libs/repoParms.sh
+++ supplimentary/libs/repoParms.sh
@@ -5,5 +5,39 @@ function repoSetParm
 	repoName="$1"
 	parameterName="$2"
 	value="$3"
-	true
+	
+	parmFile="$configDir/repos/$repoName/parameters.json"
+	touch "$parmFile"
+	
+	mass --collectionLoadArbitrary=RepoParms,"$parmFile" --set="RepoParms,$parameterName,$value"
+}
+
+function repoGetParm
+{
+	repoName="$1"
+	parameterName="$2"
+	
+	parmFile="$configDir/repos/$repoName/parameters.json"
+	touch "$parmFile"
+	
+	mass --collectionLoadArbitrary=RepoParms,"$parmFile" --get="RepoParms,$parameterName"
+}
+
+function repoGetParms
+{
+	repoName="$1"
+	
+	parmFile="$configDir/repos/$repoName/parameters.json"
+	touch "$parmFile"
+	
+	mass --collectionLoadArbitrary=RepoParms,"$parmFile" --getCategory="RepoParms" # TODO finish this
+}
+
+function showReposWithParms
+{
+	ls -1 "$configDir"/repos | while read repoName;do
+		if [ -f ""$configDir"/repos/$repoName/parameters.json" ]; then
+			echo "$repoName"
+		fi
+	done
 }
